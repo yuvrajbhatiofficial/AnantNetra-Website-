c5472d5d7a78039a722ad02b295b4003461d2d4d
Yuvraj <yuvrajbhatiofficial@gmail.com>
2025-10-06 16:58:03 +0530

    init frontend

---
+++ frontend/src/app/components/HomePage.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function HomePage() {
+  return (
+    <div>
+      <h1 className='p-4 m-2 font-bold '>hello world</h1>
+    </div>
+  )
+}
+
+export default HomePage
---
+++ frontend/src/app/page.tsx
@@ -0,0 +1,7 @@
+"use client";
+
+import HomePage from "@/app/components/HomePage";
+
+export default function Page() {
+  return <HomePage  />;
+}
